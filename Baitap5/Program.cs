ffb4a91e09c715751b4d3dfa17ec91bfd4af6a76
linhptt <linhptt@LINHPTT-KHDD>
2024-10-03 21:21:24 +0700

    Add project files.

---
+++ Baitap5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitap5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// 1.Đoc va in cac phan tu trong mang vua nhap
+            //Console.Write("Nhap vao so luong phan tu cua mang: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] array = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("Nhap phan tu thu" + (i + 1) + ": ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Mang vua nhap: " + string.Join(" ", array));
+            ////2. In mang du lieu theo chieu dao nguoc
+            //Array.Reverse(array);
+            //Console.WriteLine("Mang theo chieu dao nguoc: " + string.Join(" ", array));
+            ////3. Tim so phan tu giong nhau va so luong
+            //var ptgiong = array.GroupBy(x => x)
+            //                      .Where(g => g.Count() > 1);
+
+            //Console.WriteLine("Cac phan tu giong nhau và so luong cua chung la:");
+            //foreach (var giong in ptgiong)
+            //{
+            //    Console.WriteLine($"Gia tri {giong.Key} xuat hien {giong.Count()} lan");
+            //}
+
+            //// 4. In cac phan tu duy nhat trong mang
+            //var ptduynhat = array.GroupBy(x => x)
+            //                        .Where(g => g.Count() == 1)
+            //                        .Select(g => g.Key);
+
+            //Console.Write("Cac phan tu duy nhat trong mang:");
+            //foreach (var duynhat in ptduynhat)
+            //{
+            //    Console.Write(duynhat + " ");
+            //}
+            //Console.WriteLine();
+            ////5. Chia mang du lieu thanh mang chan va mang le
+            //var mangchan = array.Where(x => x % 2 == 0);
+            //var mangle = array.Where(x => x % 2 != 0);
+
+            //Console.Write("Cac phan tu chan trong mang: ");
+            //foreach (var chan in mangchan)
+            //{
+            //    Console.Write(chan + " ");
+            //}
+            //Console.WriteLine();
+            //Console.Write("Cac phan tu le trong mang: ");
+            //foreach (var le in mangle)
+            //{
+            //    Console.Write(le + " ");
+            //}
+            //Console.WriteLine();
+            ////6.Sap xep thu tu giam dan
+            //Array.Sort(array);
+            //Array.Reverse(array);
+            //Console.WriteLine("Mang giam dan: " + string.Join(" ", array));
+            ////7.Tim phan tu lon thu 2 trong mang
+            //if (n >= 2)
+            //{
+            //    int max = array.Max();
+            //    int secondMax = array.Where(x => x != max).Max();
+            //    Console.WriteLine($"Phan tu lon thu hai trong mang la: {secondMax}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Mang co it hon 2 phan tu.");
+            //}
+            //Console.ReadKey();
+        }
+                }
+            }
+        
+    
+
+                
+            
+        
+    
